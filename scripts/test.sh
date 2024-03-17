db243272a5ff3cbfdaf9245cd13b9c419a979c64
KanghoonYoon <ykhoon0827@gmail.com>
2024-03-17 23:53:07 +0900

    script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
 export NUM_GUP=1
 
-output_dir="checkpoints/sgcls/RETAG/MIXUP(True)BETA(20_5)_NUMRET(10)_THRESH(0.3)_MEMORY_SIZE(8)"
-model_dir="${output_dir}/model_0032500.pth"
+output_dir="retag_ckpt/predcls"
+model_dir="${output_dir}/model_final.pth"
 config_file="${output_dir}/config.yml"
 
 USE_PREDICT_BIAS=False
